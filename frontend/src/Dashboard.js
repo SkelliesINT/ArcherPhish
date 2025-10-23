5dcd3ee291caab048674c1f2b1bb59d68d4d1ee2
ikyled <143066986+ikyled@users.noreply.github.com>
2025-10-23 13:40:03 -0400

    cleaned up app

---
+++ frontend/src/Dashboard.js
@@ -0,0 +1,13 @@
+//dashboard page for ArcherPhish
+
+import './Dashboard.css';
+
+function Dashboard(){
+    return (
+        <div className='dashboard-page'>
+            <h1>Dashboard Page for ArcherPhish</h1>
+        </div>
+    );
+}
+
+export default Dashboard;
