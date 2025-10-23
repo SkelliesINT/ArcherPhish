b9027476670d8158ad9569aacc538c944340c968
ikyled <143066986+ikyled@users.noreply.github.com>
2025-10-23 13:40:03 -0400

    cleaned up app

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,1 +1,13 @@
 //Login form for ArcherPhish
+
+import './Login.css'
+
+function Login() {
+    return (
+        <div className='login-page'>
+            <h1>Login Page for ArcherPhish</h1>
+        </div>
+    );
+}
+
+export default Login;
