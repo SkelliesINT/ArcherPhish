1514dfb50304307f6eafe7043ff8b49285358497
ikyled <143066986+ikyled@users.noreply.github.com>
2025-11-08 00:14:45 -0500

    added add email/frontend for create campaign

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Login";
 import Register from "./Register";
 import Dashboard from "./Dashboard";
+import CreateCampaign from "./CreateCampaign";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -13,6 +14,7 @@ root.render(
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />@
+        <Route path="/create-campaign" element={<CreateCampaign />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
